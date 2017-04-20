b6cd773ca6e0adde6938f6cdbc3bd7686aa18d8c
filodemoro <filippo.demoro@gmail.com>
2017-04-20 22:48:33 +0200

    php registration e diritti
    ho aggiunto in tutte le pagine la belinata della scrittina dei diritti e
    ho aggiunto la funzione trim nel php e rinominato le variabili oscene
    che avevamo messo... tante belle cose

--- registration.php
+++ registration.php
@@ -5,19 +5,34 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
     $passwordConfirm = $_POST['passwordConfirm'];
-
-    $cryptpassword= crypt($password);
-
-      $somecontent = $username."-".$email."-".$cryptpassword;
-        $somecontent.= "\r\n";
     
+
+    //if controlliamo errori possibili tipo email vuota o cose del genere
+    // se non ci sono errori allora facciamo operazioni di pulizia e cose varie e intento ne scrivo alcuni che ho trovato
     
-    if($password == $passwordConfirm){
-        $myFile = "Utenti.txt";
-        $fh = fopen($myFile,'a+');
-        fwrite($fh,$somecontent);
-        fclose($myFile);
+    if(strlen($username) < 21){
+        if($password == $passwordConfirm){
+        
+            $username = trim($username);
+            $email = trim($email);
+            $password = trim($password);
+            $passwordConfirm = trim($passwordConfirm);
+
+            $cryptpassword= crypt($password);
+
+            $userData = $username."-".$email."-".$cryptpassword;
+            $userData.= "\r\n";
+        
+            $usersFile = "Users.txt";
+            $fh = fopen($usersFile,'a+');
+            fwrite($fh,$userData);
+            fclose($usersFile);
+        }
+        else
+            echo "il contenuto di conferma password è diverso dal contenuto di password ";
     }
     else
-        echo "sei un coglione";
+        echo "l'username deve avere meno di 21 caratteri";
+
+
 ?>
