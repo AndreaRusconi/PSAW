12e9890aeb9793b2e1af8b5699ac43f05b634947
filodemoro <filippo.demoro@gmail.com>
2017-05-19 18:19:05 +0200

    fatto la cacata della gallery ma non mi piace
    potrei finirlo ma c'è troppa gente che limona me ne vado a casa

--- private.php
+++ private.php
@@ -22,8 +22,8 @@ if(!isset($_SESSION['username'])){
         <li class="other"><a href="logout.php">logout</a></li>
         <li class="other"><a href="" > <?php echo $_SESSION['username'] ?> </a></li>
         <li class="barra"><a>|</a></li>
-        <li class="other"><a href="#">info</a></li>
-        <li class="other"><a href="aboutUs.php">about us</a></li>
+        <li class="other"><a href="info.html">info</a></li>
+        <li class="other"><a href="aboutUs.html">about us</a></li>
         <li class="event"><a href="index.php"><img src="CSS/Images/logo.png" height="50px" width="140px"></a></li>
     </ul>
     
