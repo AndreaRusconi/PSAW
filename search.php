41d291795e20aa8b546222ea68c72f24b3e4d92f
filodemoro <filippo.demoro@gmail.com>
2017-06-22 14:12:37 +0200

    non toccare le solite cose .. ti ho creato gia le pagine profile e generalProfile

--- search.php
+++ search.php
@@ -26,7 +26,7 @@ include("db_con.php");
 
    /*$conn = connection();
     */
-    $sql = "SELECT latitudine, longitudine FROM event";
+    $sql = "SELECT * FROM event";
    
 
 
@@ -35,32 +35,37 @@ include("db_con.php");
     $rowcount=mysqli_num_rows($result);
    // $row=mysqli_fetch_assoc($result);
   
-    echo $rowcount;
-$la;
-$lo;
+      
+    $la;
+    $lo;
     
-   if ($result->num_rows > 0) {
+    if ($result->num_rows > 0) {
          while($rowcount>0){
              
              $row[$rowcount] = mysqli_fetch_assoc($result);
              $rowcount = $rowcount - 1;
              
-             echo "sono qui";
          }
    }
 
-        
+        $i = 0;
+        $array[rowcount][5];
          
-         foreach ($row as $cord)
-{
+         foreach ($row as $cord){
              
-     $la = $cord['latitudine'];  
-    $lo = $cord['longitudine'];
+            $array[$i][0] =  $cord['nome']; 
+            $array[$i][1] =  $cord['descrizione'];
+            $array[$i][2] =  $cord['latitudine'];
+            $array[$i][3] =  $cord['longitudine'];
+            $array[$i][4] =  $cord['user'];
              
-    echo 'Titolo film: ' , $cord['latitudine'];
-    echo '<br>';
-    echo 'Anno: ' , $cord['longitudine'];
-}
+           // $la = $cord['latitudine'];  
+            //$lo = $cord['longitudine'];
+            //$descr = $cord['descrizione'];
+            //$nomi[$i] = $cord['nome'];
+            //$user = $cord['user'];
+            $i = $i + 1;
+         }
          
         //row=mysqli_fetch_assoc($result);
         
@@ -98,6 +103,12 @@ $lo;
         
         
             <li id="dataEvent">
+                
+                <div id= "nome">Seleziona un Evento...</div>
+                <div id= "descrizione"></div>
+                <div id="segnalazione"><a href="profile.php" id= "segnUser" ></a></div>
+                
+                
             </li>
             
         </ul>    
@@ -105,9 +116,73 @@ $lo;
         <script>
             function myMap() {
                 
-                var startCenter = new google.maps.LatLng(51.508742,-0.120850);
-                var mapProp= {center: startCenter ,zoom:4,};
-                var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+                
+                  
+                
+                        
+                            var startCenter = new google.maps.LatLng(44.4264000, 8.9151900);
+                            var mapProp= {center: startCenter ,zoom:11,};
+                            var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+                
+                
+                
+                
+             /*   
+                
+                if (navigator.geolocation) {
+                            navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+
+                                map.setCenter(pos);
+                        });
+                }
+                
+    */            
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                           /*  if (navigator.geolocation) {
+                
+                                alert('aono qui');
+                                navigator.geolocation.getCurrentPosition(function(get_position);
+                                alert('aono qui2');
+                
+                                }
+                            else{
+                
+                                alert('La geo-localizzazione NON è possibile');
+                                }            
+                
+                
+                
+                            function get_position(position){
+                                
+                                alert('aono qui9');
+                                var latitude = position.coords.latitude;
+                                var longitude = position.coords.longitude;
+ 
+                                var miaPosizione = new google.maps.LatLng(latitude,longitude);
+                                alert('aono qui');
+                            
+                                var marker = new google.maps.Marker({position: miaPosizione});
+                                marker.setMap(map); 
+                                
+
+                
+
+                            }
+    
               
                 
                 /*    function(marker) {          
@@ -130,24 +205,42 @@ $lo;
                 }
                 
                 */
-                var marker = new google.maps.Marker({position:startCenter});
-                marker.setMap(map);     
                 
-                /*
-                var infowindow = new google.maps.InfoWindow({content: "ciao!"});
-                infowindow.open(map,marker);
-                */
+            
+            
+            
+           
+            
+            
+            
+            
+            
+                
+                var startCenter = new google.maps.LatLng(44.4264000, 8.9151900);
+                
+                var marker = new google.maps.Marker({position:startCenter});
+                marker.setMap(map); 
+                
+                var nome = "<?php echo $array[0][0] ?>";
+                var descrizione = "<?php echo $array[0][1] ?>";
+                var coordinate = "<?php echo $array[0][2] ?> , <?php echo $array[0][3] ?>";
+                var segnalatoDa =  "segnalato da: <?php echo $array[0][4] ?>";
+               
+                
+                
                 
                 google.maps.event.addListener(marker, 'click', function() {
-                var newCenter = new google.maps.LatLng(<?php echo $cord['latitudine'] ?>,<?php echo $lo ?>);
-                marker.setPosition(newCenter);
+                    
+                   // var newCenter = new google.maps.LatLng(<?php echo $cord['latitudine'] ?>,<?php echo $lo ?>);
+                    //marker.setPosition(newCenter);
                 
-                marker.setAnimation(google.maps.Animation.BOUNCE);
-                var x = "rusco sei un babbucchione";
-                document.getElementById("dataEvent").innerHTML =x;
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    document.getElementById("nome").innerHTML =nome;
+                    document.getElementById("descrizione").innerHTML =descrizione;
+                    document.getElementById("segnUser").innerHTML =segnalatoDa;
                 });
                  
-            }
+        }
             
         </script>
         
