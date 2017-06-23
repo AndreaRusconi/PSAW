36bc4a972c6020ac4d5b8e8d498807cacc2f3522
AndreaRusconi <andrearusconi1@libero.it>
2017-06-23 18:58:10 +0200

    creata pagina di modifica con link da pagina profilo

---
+++ modify.php
@@ -0,0 +1,63 @@
+<?php
+
+// Controlla se la sessione Ã¨ stata registrata, altrimenti rimanda alla pagina di login
+// Questa prima parte dobbiamo inserirla in tutte le pagine che vogliamo proteggere con password prima di qualsiasi altra cosa
+session_start();
+
+if(!isset($_SESSION['username'])){
+    header ("location:login.php");
+}
+include("db_con.php");
+
+$conn = connection();
+
+$username = $_SESSION['username'];
+
+
+if(empty($username)){
+    $username = 'none';
+}
+
+
+if(empty($name)){
+    $name = 'none';
+}
+
+if(empty($surname)){
+    $surname = 'none';
+}
+
+?>
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="CSS/profile.css" />
+    <title>Modify</title>
+</head>
+
+<body>
+
+
+<ul id="menu">
+    <li class="other"><a href="logout.php">logout</a></li>
+    <li class="other"><a href="profile.php" > <?php echo $_SESSION['username'] ?> </a></li>
+<li class="barra"><a>|</a></li>
+<li class="other"><a href="info.html">info</a></li>
+<li class="other"><a href="aboutUs.html">about us</a></li>
+<li class="event"><a href="index.php"><img src="CSS/Images/logo.png" height="50px" width="140px"></a></li>
+</ul>
+
+
+
+<form method="get" class="modify" autocomplete="off" novalidate="">
+
+    <h1> Nome <input id="name" name="name" type="text" required="required" aria-required="true" value=""  placeholder=" <?php echo $name ?>"></h1>
+    <h2> Cognome <input id="surname" name="surname" type="text" required="required" aria-required="true" value=""  placeholder=" <?php echo $surname ?>"></h2>
+    <h3> Email <input id="email" name="email" type="text" required="required" aria-required="true" value=""  placeholder=" <?php echo $email ?>"></h3>
+
+    <input id="modifyaccount" name = "submit" type = "submit" value = "Conferma">
+
+</form>
+</body>
--- profile.php
+++ profile.php
@@ -1,33 +1,33 @@
-<?php 
-// Controlla se la sessione Ã¨ stata registrata, altrimenti rimanda alla pagina di login 
-// Questa prima parte dobbiamo inserirla in tutte le pagine che vogliamo proteggere con password prima di qualsiasi altra cosa 
-session_start(); 
+<?php
+// Controlla se la sessione Ã¨ stata registrata, altrimenti rimanda alla pagina di login
+// Questa prima parte dobbiamo inserirla in tutte le pagine che vogliamo proteggere con password prima di qualsiasi altra cosa
+session_start();
 
 if(!isset($_SESSION['username'])){
     header ("location:login.php");
 }
 include("db_con.php");
 
-    $conn = connection();
+$conn = connection();
 
-    $username = $_SESSION['username'];
+$username = $_SESSION['username'];
 
- /*   $sql = "SELECT username FROM users";
-    $result = $conn->query($sql);
-    echo "sono fuori"
-    
-    if ($result->num_rows > 0) {
-        echo "sono nel if";
-        while($row = $result->fetch_assoc()) {
-        
-            if ($row["username"] == $username){
-                $email = $row["email"];
-                $name = $row["nome"];
-                $surname = $row["cognome"];
-            }
-        }
-   }
-   */
+/*   $sql = "SELECT username FROM users";
+   $result = $conn->query($sql);
+   echo "sono fuori"
+
+   if ($result->num_rows > 0) {
+       echo "sono nel if";
+       while($row = $result->fetch_assoc()) {
+
+           if ($row["username"] == $username){
+               $email = $row["email"];
+               $name = $row["nome"];
+               $surname = $row["cognome"];
+           }
+       }
+  }
+  */
 if(empty($username)){
     $username = 'none';
 }
@@ -49,34 +49,29 @@ if(empty($surname)){
 <html>
 <head>
     <link rel="stylesheet" href="CSS/profile.css" />
-    <title>Profile</title>   
+    <title>Profile</title>
 </head>
-    
-    
-    
-    <body>
-        
-    <ul id="menu">
-        <li class="other"><a href="logout.php">logout</a></li>
-        <li class="other"><a href="profile.php" > <?php echo $_SESSION['username'] ?> </a></li>
-        <li class="barra"><a>|</a></li>
-        <li class="other"><a href="info.html">info</a></li>
-        <li class="other"><a href="aboutUs.html">about us</a></li>
-        <li class="event"><a href="index.php"><img src="CSS/Images/logo.png" height="50px" width="140px"></a></li>
-    </ul>
-    
-    <h1>
-        Ciao <?php echo $_SESSION['username'] ?>!
-    </h1>
-        
-        <ul id="dati">
-            <li class="username">Username: <?php echo $username ?></li>
-            <li class="name">Nome: <?php echo $name ?></li>
-            <li class="surname">Cognome: <?php echo $surname ?></li>
-            <li class="email">email: <?php echo $email ?></li>
-        </ul>
-        
-        
-    </body>
-    
+
+
+
+<body>
+
+<ul id="menu">
+    <li class="other"><a href="logout.php">logout</a></li>
+    <li class="other"><a href="profile.php" > <?php echo $_SESSION['username'] ?> </a></li>
+    <li class="barra"><a>|</a></li>
+    <li class="other"><a href="info.html">info</a></li>
+    <li class="other"><a href="aboutUs.html">about us</a></li>
+    <li class="event"><a href="index.php"><img src="CSS/Images/logo.png" height="50px" width="140px"></a></li>
+</ul>
+
+<h1 id="head">
+    <?php echo $name ?> <?php echo $surname ?> <a href="modify.php" class="modify">Modifica profilo </a>
+
+</h1>
+
+
+
+</body>
+
 </html>
