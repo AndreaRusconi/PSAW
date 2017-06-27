7805810f9cba59e5abff2c392e2385f8457b029e
AndreaRusconi <andrearusconi1@libero.it>
2017-06-27 15:07:27 +0200

    info

--- info.php
+++ info.php
@@ -13,7 +13,7 @@ session_start();
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
 	<title>Information</title> 
 	<link rel="stylesheet" type="text/css" href="CSS/info.css"/>
-    
+
 
 </head> 
 <body> 
@@ -28,58 +28,17 @@ session_start();
         <li class="other"><a href="aboutUs.php">about us</a></li>
         <li class="event"><a href="index.php"><img src="CSS/Images/logo.png" height="50px" width="140px"></a></li>
     </ul>
-    
-    <ul class="tit">
-        
-        <li id="link">
-            <a href="index.php">Torna alla Homepage</a>
-        </li>
-        <li id="information">
-            <h1>Information</h1>
-        </li>
-        
-    </ul>
-    
-    
-	
-	<div id="container">
-		
-		<div id="wrapper">
-			<!--
-				immagini prese da 
-				http://www.smashingmagazine.com/2008/07/11/50-remarkable-nature-wallpapers/ 
-			-->
-			<ul class="slider">
-				
-				<li>
-					<img src="CSS/Images/1.jpg" id="img1" alt="" />
-				</li>
-				<li>
-					<img src="CSS/Images/2.jpg" id="img2" alt="" />
-				</li>
-				<li>
-					<img src="CSS/Images/3.jpg" id="img3" alt="" />
-				</li>
-				<li>
-					<img src="CSS/Images/4.jpg" id="img4" alt="" />
-				</li>		
-			</ul>
-			
-		</div>
-		<div class="navigation">
-			<a href="#" class="nav_prev">Indietro</a>
-			<a href="#img1" class="nav_btn">1</a>
-			  <a href="#img2" class="nav_btn">2</a>
-			  <a href="#img3" class="nav_btn">3</a>
-			  <a href="#img4" class="nav_btn">4</a>
-			<a href="#" class="nav_next">Avanti</a>
-		</div>
-		
-		
-	</div>
-	
 
-<footer class="footer" role="contentinfo">
+
+      <ul id=instruction"></ul>
+      <li class="title">Search!</li>
+      <li class="descr">Cerca un evento nelle vicinanze o nella tua città. Premi su "Search", con la geolocalizzazione e grazie ad altri utenti, troverai diversi eventi intorno a te! </li>
+      <li class="title">Share!</li>
+      <li class="descr">Condividi un evento a cui stai partecipando! Aiuta le persone intorno a te a trovare un evento condividendo con il tasto "Share". More people more fun!</li>
+      <li class="title">Chat!</li>
+      <li class="descr">Scrivi al segnalatore! Fai domande sull'evento, su orari o su come arrivarci. </li>
+
+      <footer class="footer" role="contentinfo">
     <p class="footerCopyright">Nessun Copyright registrato. Tutti i diritti non sono riservati.</p>
 </footer>
 
